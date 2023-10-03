90052ad952e38793c35a8d4a56a71e86d327c90e
Weerapat Tangchanachaianan <wtangcha@z2t10c1.42bangkok.com>
2023-10-03 11:53:34 +0700

    init

---
+++ get_next_line.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: <wtangcha> <wtangcha@student.42bangkok.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 10:37:50 by <wtangcha>        #+#    #+#             */
+/*   Updated: 2023/10/03 11:42:34 by <wtangcha>       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+char	*get_next_line(int fd)
+{
+	FILE    *textfile;
+    char    line[?];
+	i = 0;
+
+	textfile = fopen("read.txt", "r");
+	if (textfile == NULL)
+		return (NULL);
+	return
+}
---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: <wtangcha> <wtangcha@student.42bangkok.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 10:39:05 by <wtangcha>        #+#    #+#             */
+/*   Updated: 2023/10/03 10:44:37 by <wtangcha>       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
